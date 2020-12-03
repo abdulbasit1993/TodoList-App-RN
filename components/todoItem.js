41695f5ac075e35969cdf1e06bab6cd4c8ad8b2c
Abdul Basit Mehtab <abasitm.1993@gmail.com>
2020-12-03 21:44:46 +0500

    Initial commit

---
+++ components/todoItem.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+export default function TodoItem({ item, pressHandler }) {
+
+    return (
+        <TouchableOpacity onPress={() => pressHandler(item.key)}>
+            <View style={styles.item}>
+                <Icon name='delete' size={18} color='#333' />
+                <Text style={styles.itemText}>{item.text}</Text>
+            </View>
+        </TouchableOpacity>
+    )
+
+}
+
+const styles = StyleSheet.create({
+    item: {
+        padding: 16,
+        marginTop: 16,
+        borderColor: '#bbb',
+        borderWidth: 1,
+        borderStyle: 'dashed',
+        borderRadius: 10,
+        flexDirection: 'row',
+    },
+    itemText: {
+        marginLeft: 10,
+    }
+})
